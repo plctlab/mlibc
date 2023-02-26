d1431c983edfe9421da5d762ad3957659bbf2a29
linshire <89723088+linshire@users.noreply.github.com>
2023-02-26 22:56:17 +0800

    Update stdarg.h

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,9 +1,9 @@
 #ifndef __STDARG_H__
 #define __STDARG_H__
 
-#define va_list        __builtin_va_list 
+#define va_list       __builtin_va_list 
 #define va_start(v,l)	__builtin_va_start(v,l)
-#define va_end(v)		__builtin_va_end(v)
+#define va_end(v)		  __builtin_va_end(v)
 #define va_arg(v,l)		__builtin_va_arg(v,l)
 #define va_copy(d,s)	__builtin_va_copy(d,s)
 
