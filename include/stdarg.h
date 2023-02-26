af8086aff62f39c4607f5a127eb310f9c0fb0335
linshire <1127384500@qq.com>
2023-02-26 21:45:35 +0800

    删掉了一个多余的分号

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,7 +1,7 @@
 #ifndef __STDARG_H__
 #define __STDARG_H__
 
-#define va_list;        __builtin_va_list 
+#define va_list        __builtin_va_list 
 #define va_start(v,l)	__builtin_va_start(v,l)
 #define va_end(v)		__builtin_va_end(v)
 #define va_arg(v,l)		__builtin_va_arg(v,l)
