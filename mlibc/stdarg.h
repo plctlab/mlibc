57d0bb297c32e0510c811959ced9ef6e67b06d05
linshire_Ubuntu <1127384500@qq.com>
2023-04-06 00:29:40 +0800

    删掉一些没必要提交的文件

--- mlibc/stdarg.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STDARG_H__
-#define __STDARG_H__
-
-#define va_list     __builtin_va_list
-#define va_start    __builtin_va_start
-#define va_end      __builtin_va_end
-#define va_arg      __builtin_va_arg
-#define va_copy     __builtin_va_copy
-#endif
