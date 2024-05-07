b044b76a78ccd06a420c6c8e895bd41407270e69
2310863495@qq.com <2310863495@qq.com>
2024-05-07 18:29:01 +0800

    add copyright to libc.h

--- src/internal/libc.h
+++ src/internal/libc.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) mlibc & plct lab
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024/5/7   0Bitbiscuits  the first version
+ */
 #ifndef MLIBC_LIBC_H__
 #define MLIBC_LIBC_H__
 
