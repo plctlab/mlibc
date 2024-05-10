96a098cebac8ebbac54f0ba4c568caade53ffc74
2310863495@qq.com <2310863495@qq.com>
2024-05-10 20:12:35 +0800

    rm pthread mod and add some func at string mod

--- include/sys/syscall.h
+++ include/sys/syscall.h
@@ -13,25 +13,10 @@
 
 #include <sys/types.h>
 #include <sys_fileops.h>
-#include <sys_thread.h>
 
 /* system-related */
 #ifdef MLIBC_USING_FS
 typedef mlibc_file_t file_t;
 #endif
-
-#ifdef MLIBC_USING_THREAD
-typedef mlibc_timer_t timer_t;
-typedef mlibc_thread_t thread_t;
-
-#ifdef MLIBC_USING_MUTEX
-typedef mlibc_mutex_t mutex_t;
-#endif /* MLIBC_USING_MUTEX */
-
-#ifdef MLIBC_USING_SEMAPHORE
-typedef mlibc_semaphore_t sem_t;
-#endif /* MLIBC_USING_SEMAPHORE */
-
-#endif /* MLIBC_USING_THREAD */
 
 #endif /* MLIBC_SYSCALL_H__ */
