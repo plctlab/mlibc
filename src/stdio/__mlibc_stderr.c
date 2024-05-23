0cce3610f8c11db92b086733344ca0ca7e08187f
2310863495@qq.com <2310863495@qq.com>
2024-05-23 20:07:54 +0800

    add copyright

--- src/stdio/__mlibc_stderr.c
+++ src/stdio/__mlibc_stderr.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) mlibc & plct lab
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024/5/23   0Bitbiscuits  the first version
+ */
 #include "../internal/stdio_impl.h"
 #include <fcntl.h>
 
--- system/rtthread/sys_mem/__mlibc_sbrk.c
+++ system/rtthread/sys_mem/__mlibc_sbrk.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) mlibc & plct lab
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024/5/23   0Bitbiscuits  the first version
+ */
 #include <libc_config.h>
 
 #ifdef MLIBC_RUNNING_RTTHREAD
