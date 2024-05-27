7d1eb50f80037c6fbb3bca1148ac523481f87afd
2310863495@qq.com <2310863495@qq.com>
2024-05-27 17:45:25 +0800

    adjust code

--- src/stdio/__mlibc_fmode.c
+++ src/stdio/__mlibc_fmode.c
@@ -6,18 +6,18 @@ int __mlibc_fmode_to_flags(const char *mode)
 {
     int flags = 0;
 
-	if (strchr(mode, '+'))
+    if (strchr(mode, '+'))
         flags = O_RDWR;
-	else if (*mode == 'r')
+    else if (*mode == 'r')
         flags = O_RDONLY;
-	else
+    else
         flags = O_WRONLY;
 
-	if (*mode != 'r') flags |= O_CREAT;
+    if (*mode != 'r') flags |= O_CREAT;
 
-	if (*mode == 'w') flags |= O_TRUNC;
+    if (*mode == 'w') flags |= O_TRUNC;
 
-	if (*mode == 'a') flags |= O_APPEND;
-
-	return flags;
+    if (*mode == 'a') flags |= O_APPEND;
+	
+    return flags;
 }
