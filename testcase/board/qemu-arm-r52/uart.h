d7130e9d01a4c314f5225fc56765a56410a53506
2310863495@qq.com <2310863495@qq.com>
2024-07-16 13:44:38 +0800

    move uart config function to uart.h

--- testcase/board/qemu-arm-r52/uart.h
+++ testcase/board/qemu-arm-r52/uart.h
@@ -10,6 +10,8 @@
 #ifndef MLIBC_UART0_H__
 #define MLIBC_UART0_H__
 
+#include <compiler.h>
+
 #define PL011_UART0_BASE    0xE7C00000ul
 #define __REG32(x)          (*((volatile unsigned int *)(x)))
 #define UART_DR(base)   __REG32(base + 0x00)
@@ -17,5 +19,18 @@
 #define UART_CR(base)   __REG32(base + 0x08)
 #define UART_IMSC(base) __REG32(base + 0x0C)
 #define UART_ICR(base)  __REG32(base + 0x0C)
+
+mlibc_inline void UartStdOutInit(void){
+    /* enable Rx and Tx of UART */
+    UART_CR(PL011_UART0_BASE) = (1 << 0);
+}
+
+mlibc_inline unsigned char UartPutc(unsigned char my_ch)
+{
+    while (UART_FR(PL011_UART0_BASE) & (1 << 5u));
+    UART_DR(PL011_UART0_BASE) = my_ch;
+
+    return my_ch;
+}
 
 #endif /* MLIBC_UART0_H__ */
--- testcase/board/qemu-virt-aarch64/uart.h
+++ testcase/board/qemu-virt-aarch64/uart.h
@@ -10,6 +10,8 @@
 #ifndef MLIBC_UART0_H__
 #define MLIBC_UART0_H__
 
+#include <compiler.h>
+
 #define PL011_UART0_BASE    0x09000000
 #define __REG32(x)          (*((volatile unsigned int *)(x)))
 #define UART_DR(base)   __REG32(base + 0x00)
@@ -17,5 +19,18 @@
 #define UART_CR(base)   __REG32(base + 0x30)
 #define UART_IMSC(base) __REG32(base + 0x38)
 #define UART_ICR(base)  __REG32(base + 0x44)
+
+mlibc_inline void UartStdOutInit(void){
+    /* enable Rx and Tx of UART */
+    UART_CR(PL011_UART0_BASE) = (1 << 0);
+}
+
+mlibc_inline unsigned char UartPutc(unsigned char my_ch)
+{
+    while (UART_FR(PL011_UART0_BASE) & (1 << 5u));
+    UART_DR(PL011_UART0_BASE) = my_ch;
+
+    return my_ch;
+}
 
 #endif /* MLIBC_UART0_H__ */
