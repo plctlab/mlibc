c8ac026b1cb9fd803175a63c6b36c90a9a536c9a
2310863495@qq.com <2310863495@qq.com>
2024-08-20 17:42:50 +0800

    adjust file lock

--- src/internal/stdio_impl.h
+++ src/internal/stdio_impl.h
@@ -13,11 +13,17 @@
 #include <stdio.h>
 #include "lock.h"
 
-#define FLOCK_INIT(f)   __lock_init(f->_flock)
-#define FLOCK_DEINIT(f) __lock_deinit(f->_flock)
-#define FLOCK(f)        __lock_take(f->_flock)
-#define FUNLOCK(f)      __lock_release(f->_flock)
+/* File lock ops */
+#define FLOCK_INIT(f)   __lock_init_recursive(f->_flock)
+#define FLOCK_DEINIT(f) __lock_deinit_recursive(f->_flock)
+#define FLOCK(f)                                    \
+    do{                                             \
+        if(!f->_flock) __lock_init_recursive(f->_flock);    \
+        __lock_take_recursive(f->_flock);           \
+    }while(0)
+#define FUNLOCK(f)      __lock_release_recursive(f->_flock)
 
+/* Definition of FILE */
 struct __MLIBC_IO_FILE{
     int fd;
 
@@ -36,7 +42,7 @@ struct __MLIBC_IO_FILE{
 
     /* line buffered mode */
     int lbf;
-    char *getln_buf;
+    char *getln_buf; // unused
 
     /* file list */
     FILE *prev, *next;
@@ -51,21 +57,31 @@ struct __MLIBC_IO_FILE{
     off_t off;
 };
 
+/* FILE ops */
 int __mlibc_close(FILE *);
 ssize_t __mlibc_read(FILE *, unsigned char *, size_t);
 ssize_t __mlibc_write(FILE *, const unsigned char *, size_t);
-
 off_t __mlibc_lseek(FILE *, off_t, int);
-
-size_t __mlibc_toread(FILE *);
-size_t __mlibc_towrite(FILE *);
-
+/* Init read/write buffer */
+int __mlibc_toread(FILE *);
+int __mlibc_towrite(FILE *);
+/* Clean up the resources used by stdio(todo) */
 void __mlibc_stdio_exit(void);
-
+/* Change the mode(string) to file flags */
+int __mlibc_fmode_to_flags(const char *mode);
+/* FILE list operation */
 FILE *__mlibc_file_add(FILE *f);
 void __mlibc_file_remove(FILE *f);
 FILE **__ofl_lock(void);
 void __ofl_unlock(void);
-int __mlibc_fmode_to_flags(const char *mode);
+/* The macro of getc/putc(unlocked) */
+int __mlibc_uflow(FILE *f);
+int __mlibc_overflow(FILE *f, int _c);
+#define getc_unlocked(f) \
+    ( ((f)->rpos != (f)->rend) ? *(f)->rpos++ : __mlibc_uflow((f)) )
+#define putc_unlocked(c, f) \
+    ( (((unsigned char)(c) != (f)->lbf && (f)->wpos != (f)->wend)) \
+    ? *(f)->wpos++ = (unsigned char)(c) \
+    : __mlibc_overflow((f),(unsigned char)(c)) )
 
 #endif /* MLIBC_STDIO_IMPL_H__ */
