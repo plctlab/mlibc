fb64419e41531df0fafb4a0633c5a8fc98464cd2
0BitBiscuits <2310863495@qq.com>
2024-11-15 00:25:36 +0800

    fix a bug at ftrylockfile(patch)

--- src/internal/stdio_impl.h
+++ src/internal/stdio_impl.h
@@ -19,12 +19,12 @@
 #define FLOCK(f)                                    \
     do{                                             \
         if(!f->_flock) __lock_init_recursive(f->_flock);    \
-        return __lock_take_recursive(f->_flock);           \
+        __lock_take_recursive(f->_flock);           \
     }while(0)
 #define FTRYLOCK(f)                                    \
     do{                                             \
         if(!f->_flock) __lock_init_recursive(f->_flock);    \
-        __lock_trytake_recursive(f->_flock);           \
+        return __lock_trytake_recursive(f->_flock);           \
     }while(0)
 #define FUNLOCK(f)      __lock_release_recursive(f->_flock)
 
