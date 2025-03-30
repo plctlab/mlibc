e43fa0c2ce42d9ffea32edd99b5931f62ed907fa
Chris <2293611667@qq.com>
2025-03-30 16:43:43 +0800

    fix : relocate QEMU example binaries to build directory
    This change moves the QEMU example binaries from the source tree
    to the build directory, following standard build practices.
    
    Key changes:
    - Created dedicated QEMU_ELF_PATH for output binaries
    - Updated qemu-hello.mk to generate files in the build directory
    - Fixed all qemu.bat scripts to use the new binary locations
    
    Signed-off-by: Jiajie Zhou 2293611667@qq.com

--- helloworld/qemu/qemu-vexpress-a9/qemu.bat
+++ helloworld/qemu/qemu-vexpress-a9/qemu.bat
@@ -1,2 +1,2 @@
 @echo off
-qemu-system-arm -M vexpress-a9 -kernel qemu-vexpress-a9.elf -serial stdio -m 512
+qemu-system-arm -M vexpress-a9 -kernel ../../../build/arm/qemu/qemu-vexpress-a9/qemu-vexpress-a9.elf -serial stdio -m 512
